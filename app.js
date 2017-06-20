70fd71bdd20bb7a81f7f0d0179fd1d0a17b5497a
Shubham Singhal <torquecode@gmail.com>
2017-06-20 21:02:57 +0530

    socket.io listener added

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ var path = require('path');
 var app = express();
 var port = process.env.PORT || 3000;
 
+
 app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
 app.set('views', path.join(__dirname + '/views'));
 
+var server = app.listen(port, function () {
+  console.log('server listening on port ' + port);
+});
 
+var io = require('socket.io')(server);
+
+// Socket.io listener
+io.on('connection', function(client) {
+    console.log('a user connected');
+    client.on('disconnect', function() {
+        console.log('a user disconnected');
+    });
+});
 
 app.get('/', function(req, res) {
     res.sendFile(app.get('views') + '/index.html');
-})
-
-app.listen(port, function () {
-    console.log('app listening on port ' + port);
 })
