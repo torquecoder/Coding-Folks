91f02d2d3bf762e137c5501cbc4af38529798a9f
Shubham Singhal <torquecode@gmail.com>
2017-06-21 14:18:38 +0530

    connected with rethinkdb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var rdb = require('rethinkdb');
+var r = require('rethinkdb');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -9,8 +9,28 @@ app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.set('views', path.join(__dirname + '/views'));
 
-var server = app.listen(port, function () {
+var server = app.listen(port, function() {
   console.log('server listening on port ' + port);
+});
+
+// Connection with rethinkdb
+r.connect({
+    host: 'localhost',
+    port: 28015
+}, function(err, conn) {
+    if(err)
+        throw err;
+    r.db('test').tableList().run(conn, function(err, response) {
+        if(response.indexOf('edit') > -1) {
+            // Table exists
+            console.log('Table exists, skipping create...');
+            console.log('Tables - ' + response);
+        } else {
+            // Create table
+            console.log('Table does not exist. Creating');
+            r.db('test').tableCreate('edit').run(conn);
+        }
+    });
 });
 
 // Socket.io listener
