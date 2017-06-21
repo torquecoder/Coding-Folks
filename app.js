045d195ea5727c4dba69f5aa30ef8a3ce020b0ce
Shubham Singhal <torquecode@gmail.com>
2017-06-21 14:06:42 +0530

    code refactored

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 var express = require('express');
 var path = require('path');
+var rdb = require('rethinkdb');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
-
 app.set('views', path.join(__dirname + '/views'));
 
 var server = app.listen(port, function () {
   console.log('server listening on port ' + port);
 });
 
-var io = require('socket.io')(server);
-
 // Socket.io listener
+var io = require('socket.io')(server);
 io.on('connection', function(client) {
     console.log('a user connected');
     client.on('disconnect', function() {
