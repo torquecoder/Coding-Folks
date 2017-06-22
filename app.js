1a163babbce56eab215497c73fda9698316be512
Shubham Singhal <torquecode@gmail.com>
2017-06-22 21:01:57 +0530

    rethink magic added :)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('node_modules'));
 app.set('views', path.join(__dirname + '/views'));
 
 var server = app.listen(port, function() {
-  console.log('server listening on port ' + port);
+    console.log('server listening on port ' + port);
 });
 
 // Connection with rethinkdb
@@ -18,10 +18,10 @@ r.connect({
     host: 'localhost',
     port: 28015
 }, function(err, conn) {
-    if(err)
+    if (err)
         throw err;
     r.db('test').tableList().run(conn, function(err, response) {
-        if(response.indexOf('edit') > -1) {
+        if (response.indexOf('edit') > -1) {
             // Table exists
             console.log('Table exists, skipping create...');
             console.log('Tables - ' + response);
@@ -40,7 +40,33 @@ io.on('connection', function(client) {
     client.on('disconnect', function() {
         console.log('a user disconnected');
     });
+
+    socket.on('document-update', function(msg) {
+        console.log(msg);
+        r.table('edit').insert({
+            id: msg.id,
+            value: msg.value,
+            user: msg.user
+        }, {
+            conflict: "update"
+        }).run(conn, function(err, res) {
+            if (err)
+                throw err;
+        });
+    });
+
+    r.table('edit').changes().run(conn, function(err, cursor) {
+        if (err) throw err;
+        cursor.each(function(err, row) {
+            if (err) throw err;
+            io.emit('doc', row);
+        });
+    });
+
 });
+
+
+
 
 app.get('/', function(req, res) {
     res.sendFile(app.get('views') + '/index.html');
